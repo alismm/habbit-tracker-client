b0ae93bebbf4ef41765fdb7a581da8f7a453d9c4
ahmad.mousavi <ahmoosavi1396@gmail.com>
2023-09-11 15:03:33 +0330

    feat(home): add navbar component to dashboard layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,27 @@
 <template>
-  <p>Home</p>
+  <DashboardLayout>
+    <template #navigation>
+      <NavbarItem>
+      </NavbarItem>
+    </template>
+    <template #main>
+      <p>a</p>
+    </template>
+    <template #leftSideBar>
+      <p>a</p>
+    </template>
+  </DashboardLayout>
 </template>
 
 <script>
+import DashboardLayout from "@/layouts/DashboardLayout.vue"
+import NavbarItem from "@/common/Navbar.vue"
+
 export default {
-  name: 'AppHome'
+  name: 'AppHome',
+  components: {
+    DashboardLayout,
+    NavbarItem
+  }
 }
 </script>
