957a88327a1c91ab67e0db50c9e1f047bf2f7721
ali.saeedimanesh <alisaeediasm2000@yahoo.com>
2023-09-11 15:10:23 +0330

    refactor(global): refactor components
    -refactor Textarea components
    -refactor CreateActivity components
    -refactor HorizontalScroll

--- src/common/BaseIcon.vue
+++ src/common/BaseIcon.vue
@@ -6,10 +6,19 @@
 
   import AddCircleIcon from "@/common/icons/AddCircleIcon.vue"
   import WeightIcon from "@/common/icons/WeightIcon.vue"
-  
+  import NoteFillIcon from "@/common/icons/NoteFillIcon.vue"
+  import NoteIcon from "@/common/icons/NoteIcon.vue"  
+  import CalenderIcon from "@/common/icons/CalenderIcon.vue"
+  import ClockIcon from "@/common/icons/ClockIcon.vue"
+  import DocumentIcon from "@/common/icons/DocumentIcon.vue"
   const icons = {
     addCircle:AddCircleIcon,
     weight:WeightIcon,
+    noteFill:NoteFillIcon,
+    note:NoteIcon,
+    calender:CalenderIcon,
+    clock:ClockIcon,
+    Document:DocumentIcon
   };
   
   export default {
@@ -18,9 +27,9 @@
       name: {
         type: String,
         required: true,
-        validator(value) {
-          return Object.prototype.hasOwnProperty.call(value, icons);
-        },
+        // validator(value) {
+        //   return Object.prototype.hasOwnProperty.call(value, icons);
+        // },
       },
     },
     computed: {
---
+++ src/common/modals/CreateActivity.vue
@@ -0,0 +1,131 @@
+<template>
+  <ModalLayout>
+    <div class="create-activity">
+      <!-- heading -->
+      <h3 class="create-activity__heading">ایجاد فعالیت</h3>
+      <!-- form activity type -->
+      <HorizontalScroll gap="1.2rem" width="9.6rem" height="9.6rem">
+        <template #svgHorizontalScroll>
+            <a class="horizontal-scroll__item">
+            <img id="img-add" src="../../../old-project/assets/images/Frame-plus.svg" alt="" />
+            </a>
+        </template>
+        <template #imgHorizontalScroll>
+            <a class="horizontal-scroll__item">
+            <img src="../../old-project/assets/images/create-activity-img1.png" alt="" />
+          </a>
+        </template>
+      </HorizontalScroll>
+      <section class="create-activity__form-container">
+      <vee-form class="form">
+        <InputItem nameInput="createActivity"
+          >نام فعالیت
+          <template #inputIcon>
+            <BaseIcon name="note"></BaseIcon>
+          </template>
+        </InputItem>
+
+        <TextAreaItem>
+
+        </TextAreaItem>
+        <InputItem nameInput="date">
+          تاریخ
+          <template #inputIcon>
+            <BaseIcon name="calender"></BaseIcon>
+          </template>
+        </InputItem>
+        <div class="form__double-input">
+          <InputItem nameInput="startTime"
+            >از ساعت 
+            <template #inputIcon>
+              <BaseIcon name="clock"></BaseIcon>
+            </template>
+          </InputItem>
+          <InputItem nameInput="endTime"
+            >تا ساعت 
+            <template #inputIcon>
+              <BaseIcon name="clock"></BaseIcon>
+            </template>
+          </InputItem>
+        </div>
+        <InputItem nameInput="create"
+          >نوع فعالیت
+          <template #inputIcon>
+            <BaseIcon name="weight"></BaseIcon>
+          </template>
+        </InputItem>
+        <div class="form__button-container">
+          <ButtonItem styleButton="button_secondary"> لغو </ButtonItem>
+          <ButtonItem styleButton="button_primary" type="submit"> ایجاد </ButtonItem>
+        </div>
+      </vee-form>
+    </section>
+    </div>
+  </ModalLayout>
+</template>
+
+<script>
+import ModalLayout from '@/layouts/ModalLayout.vue'
+import InputItem from '@/common/Input.vue'
+import ButtonItem from '@/common/Button.vue'
+import BaseIcon from '@/common/BaseIcon.vue'
+import HorizontalScroll from '@/common/HorizontalScroll.vue'
+import TextAreaItem from '@/common/Textarea.vue'
+
+export default {
+  components: {
+    ModalLayout,
+    InputItem,
+    ButtonItem,
+    BaseIcon,
+    HorizontalScroll,
+    TextAreaItem
+  },
+  methods: {
+   previewFiles(event) {
+      console.log(event.target.files);
+   }
+}
+}
+</script>
+
+<style lang="scss">
+.create-activity {
+  background: var(--theme-background);
+  border-radius: 2rem;
+  max-width: 38rem;
+  padding: 3.2rem 0 4rem 0;
+  width: 100%;
+  overflow: hidden;
+
+  &__heading {
+    margin-bottom: 3.2rem;
+    text-align: center;
+    @include typography("text-18-32-bold-900"); 
+  }
+  
+  &__form-container {
+    margin-right: 2.4rem;
+    margin-left: 2.4rem;
+    margin-top: 2.4rem;
+
+    display: flex;
+    flex-direction: column;
+    gap: 1.6rem;
+  }
+}
+.form {
+  display: flex;
+  flex-direction: column;
+  gap: 1.6rem;
+
+  &__double-input {
+    @include box-container-direction-row(0.8rem);
+  }
+
+  &__button-container {
+    @extend .form__double-input;
+    margin-top: 2.4rem;
+  }
+}
+</style>
